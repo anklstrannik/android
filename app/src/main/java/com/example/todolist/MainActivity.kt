36b5e64f5a69a9d4397221202c529ef9c959b77e
Alex Borisovec <yadyadyastrannik85@gmail.com>
2024-01-16 17:48:09 +0300

    add if else

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,16 +2,21 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
+import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var stubContainer: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // getting the recyclerview by its id
         val recyclerview = findViewById<RecyclerView>(R.id.main_recycler_view)
+        stubContainer = findViewById(R.id.main_no_items_container)
 
         // this creates a vertical layout Manager
         recyclerview.layoutManager = LinearLayoutManager(this)
@@ -21,8 +26,17 @@ class MainActivity : AppCompatActivity() {
 
         // This loop will create 20 Views containing
         // the image with the count of view
-        for (item in 1..40) {
-            data.add(ToDoItem("title", "description", item))
+        //for (item in 1..40) {
+        //    data.add(ToDoItem("title", "description", item))
+        //}
+
+        if (data.isEmpty()){
+            stubContainer.visibility = VISIBLE
+            recyclerview.visibility = INVISIBLE
+        }
+        else{
+            stubContainer.visibility = INVISIBLE
+            recyclerview.visibility = VISIBLE
         }
 
         // This will pass the ArrayList to our Adapter
